f4a54580977f58be7c51319ef4b2d9d31f200885
marcusklasenius <marcusklasenius@hotmail.com>
2021-04-19 18:42:53 +0200

    Added choose function

--- application/commands/serve.py
+++ application/commands/serve.py
@@ -11,24 +11,19 @@ def serve(options):
     # Create a Flask application
     app = Flask(__name__,template_folder="templates")
 
+    covid_data_frame = pd.read_csv("jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-15-2021.csv", dtype="category", sep=",")
+    urban_data_frame = pd.read_csv("urban_data/share-of-population-urban.csv", dtype="category", sep=",")
+    countries = covid_data_frame["Country_Region"].cat.categories
+    country_links = [item.replace(" ", "") for item in countries]
+
     @app.route("/")
     def index():
         """Return the index page of the website."""
         return send_file("../www/index.html")
 
-    @app.route("/greeting/<name>")
-    def greeting(name):
-        """Return a greeting for the user."""
-        return "Hello, {}!".format(name)
-
-
     @app.route("/deathspercountry")
     def deathspercountry():
-        """Return an page showing urbanisation data"""
-        
-        covid_data_frame = pd.read_csv("jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-15-2021.csv", dtype="category", sep=",")
-        countries = covid_data_frame["Country_Region"].cat.categories
-
+        """Return an page showing death data"""
         data_list = []
 
         for country in countries:
@@ -39,18 +34,37 @@ def serve(options):
             data_list.append(country + " - " + str(country_deaths))
         return render_template("deathspercountry.html",data_list=data_list)
 
-    @app.route("/goodbye")
-    def goodbye(name):
-        """Return a greeting for the user."""
-        return "Goodbye, {}!".format(name)
+    @app.route("/choose")
+    def choose():
+        """Return the choice page"""
+        return render_template("choose.html",countries=countries,country_links=country_links)
+
+    @app.route("/data/<country>")
+    def country(country):
+        """Return a summarization of the choosen country"""
+        country_index = country_links.index(country)
+        country_name = countries[country_index]
+        country_deaths = 0
+        country_cases = 0
+        country_data = covid_data_frame.loc[(covid_data_frame["Country_Region"] == country_name)]
+
+        for value in country_data["Deaths"].values:
+            country_deaths += int(value)
+        for value in country_data["Confirmed"].values:
+            country_cases += int(value)   
+
+        try:
+            urban_data = urban_data_frame.loc[(urban_data_frame["Entity"] == country_name) & ((urban_data_frame["Year"] == "2017"))]
+            urban_population = float(urban_data["Urban population (% of total)"].values[0])
+            return f"{country_name} has {country_cases} confirmed cases and {country_deaths} deaths. {urban_population}% of {country_name} is urbanised."
+        except:
+            return f"{country_name} has {country_cases} confirmed cases and {country_deaths} deaths. Urbanization data is missing"
 
 
     def arrange(contries):
         un_arranged_file = open("jhdata\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports\04-15-2021.csv", "r")
         line_to_be_split = un_arranged_file.readline()
         current_line_list = line_to_be_split.split(',')
-
-
 
     app.run(host=options.address, port=options.port, debug=True)
 
