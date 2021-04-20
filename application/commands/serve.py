480ad976a80247cdc8beb2dd7086ef0250784717
Moo4Cash <staticrook77@gmail.com>
2021-04-20 11:56:01 +0200

    test

--- application/commands/serve.py
+++ application/commands/serve.py
@@ -1,10 +1,9 @@
 """Module for serving an API."""
 
 from flask import Flask, send_file, render_template
-
 import pandas as pd
-
 import csv
+
 def serve(options):
     """Serve an API."""
 
@@ -15,6 +14,7 @@ def serve(options):
     def index():
         """Return the index page of the website."""
         return send_file("../www/index.html")
+
 
     @app.route("/greeting/<name>")
     def greeting(name):
@@ -39,10 +39,27 @@ def serve(options):
             data_list.append(country + " - " + str(country_deaths))
         return render_template("deathspercountry.html",data_list=data_list)
 
-    @app.route("/goodbye")
-    def goodbye(name):
-        """Return a greeting for the user."""
-        return "Goodbye, {}!".format(name)
+
+    @app.route("/newestdata")
+    def newestdata():
+        """Return a table of data."""
+
+        file_path = "jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-15-2021.csv"
+        with open(file_path, newline="") as f:
+            data = list(csv.reader(f))
+
+        return render_template("newestdata.html",data=data)
+
+    
+    @app.route("/newestdataus")
+    def newestdataus():
+        """Return a table of data."""
+
+        file_path = "jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-15-2021.csv"
+        with open(file_path, newline="") as f:
+            data = list(csv.reader(f))
+
+        return render_template("newestdata.html",data=data)
 
 
     def arrange(contries):
@@ -50,9 +67,8 @@ def serve(options):
         line_to_be_split = un_arranged_file.readline()
         current_line_list = line_to_be_split.split(',')
 
-
-
     app.run(host=options.address, port=options.port, debug=True)
+
 
 def create_parser(subparsers):
     """Create an argument parser for the "serve" command."""
