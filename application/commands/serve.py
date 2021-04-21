d44a6b7ce8c0623e8afaa5c13b6b26d346f84028
marcusklasenius <marcusklasenius@hotmail.com>
2021-04-21 21:56:31 +0200

    kommentar

--- application/commands/serve.py
+++ application/commands/serve.py
@@ -1,6 +1,6 @@
 """Module for serving an API."""
 
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, redirect
 import pandas as pd
 import csv
 
@@ -8,39 +8,24 @@ def serve(options):
     """Serve an API."""
 
     # Create a Flask application
-    app = Flask(__name__,template_folder="templates")
+    app = Flask(__name__,template_folder="templates",static_folder="static")
 
-    covid_data_frame = pd.read_csv("jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-15-2021.csv", dtype="category", sep=",")
-    urban_data_frame = pd.read_csv("urban_data/share-of-population-urban.csv", dtype="category", sep=",")
+    covid_data_frame = pd.read_csv("data/jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-15-2021.csv", dtype="category", sep=",")
+    urban_data_frame = pd.read_csv("data/urban_data/share-of-population-urban.csv", dtype="category", sep=",")
     countries = covid_data_frame["Country_Region"].cat.categories
-    country_links = [item.replace(" ", "") for item in countries]
+    country_links = [(item.replace(" ", "")).lower() for item in countries]
 
     @app.route("/")
     def index():
         """Return the index page of the website."""
-        return send_file("../www/index.html")
+        return render_template("index.html",countries=countries,country_links=country_links)
 
-    @app.route("/deathspercountry")
-    def deathspercountry():
-        """Return an page showing death data"""
-        data_list = []
-
-        for country in countries:
-            country_deaths = 0
-            country_data = covid_data_frame.loc[(covid_data_frame["Country_Region"] == country)]
-            for value in country_data["Deaths"].values:
-                country_deaths += int(value)
-            data_list.append(country + " - " + str(country_deaths))
-        return render_template("deathspercountry.html",data_list=data_list)
-
-    @app.route("/choose")
-    def choose():
-        """Return the choice page"""
-        return render_template("choose.html",countries=countries,country_links=country_links)
-
-    @app.route("/data/<country>")
+    @app.route("/<country>")
     def country(country):
         """Return a summarization of the choosen country"""
+        if country == "Choose country":
+            return redirect("/data")
+
         country_index = country_links.index(country)
         country_name = countries[country_index]
         country_deaths = 0
@@ -55,41 +40,21 @@ def serve(options):
         try:
             urban_data = urban_data_frame.loc[(urban_data_frame["Entity"] == country_name) & ((urban_data_frame["Year"] == "2017"))]
             urban_population = float(urban_data["Urban population (% of total)"].values[0])
-            return f"{country_name} has {country_cases} confirmed cases and {country_deaths} deaths. {urban_population}% of {country_name} is urbanised."
+            string = f"{country_name} has {country_cases} confirmed cases and {country_deaths} deaths. {urban_population}% of {country_name} is urbanised."
         except:
-            return f"{country_name} has {country_cases} confirmed cases and {country_deaths} deaths. Urbanization data is missing"
+            string = f"{country_name} has {country_cases} confirmed cases and {country_deaths} deaths. Urbanization data is missing"
 
-    @app.route("/newestdata")
-    def newestdata():
+        return render_template("country.html",country=country_name,string=string)
+
+    @app.route("/data")
+    def data():
         """Return a table of data."""
 
-        file_path = "jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-15-2021.csv"
+        file_path = "data/jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-15-2021.csv"
         with open(file_path, newline="") as f:
             data = list(csv.reader(f))
 
-        return render_template("newestdata.html",data=data)
-
-    @app.route("/newestdataus")
-    def newestdataus():
-        """Return a table of data."""
-
-        file_path = "jhdata/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-15-2021.csv"
-        with open(file_path, newline="") as f:
-            data = list(csv.reader(f))
-
-        return render_template("newestdata.html",data=data)
-
-    @app.route("/casespercapita")
-    def goodbye(name):
-        """Display a list showing the cases per capita for the countries"""
-        return "Goodbye, {}!".format(name)
-
-
-
-    def arrange(contries):
-        un_arranged_file = open("jhdata\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports\04-15-2021.csv", "r")
-        line_to_be_split = un_arranged_file.readline()
-        current_line_list = line_to_be_split.split(',')
+        return render_template("data.html",data=data)
 
     app.run(host=options.address, port=options.port, debug=True)
 
